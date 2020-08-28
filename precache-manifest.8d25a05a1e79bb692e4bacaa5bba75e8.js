900ccd1a97001cd233f1f1861f6147ab8b009da2
Qamer Abbas <qamerawan98@gmail.com>
2020-08-29 02:32:43 +0500

    Updates

---
+++ precache-manifest.8d25a05a1e79bb692e4bacaa5bba75e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c3a0b3dec03d3529c6d4a3d5134ece2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c87e46d2cd0f1e41f2a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9fc4771ba1b23a4e346f",
+    "url": "/static/js/2.013394d4.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.013394d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c87e46d2cd0f1e41f2a",
+    "url": "/static/js/main.87eeefd2.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
